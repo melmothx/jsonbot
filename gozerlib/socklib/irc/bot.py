bab70aa92ad1d00ca1c42626aa973297a07219e0
"Bart Thate ext:(%22) <bthate@gmail.com>
2010-05-14 13:57:29 +0200

    make reboot on single bot work

--- gozerlib/socklib/irc/bot.py
+++ gozerlib/socklib/irc/bot.py
@@ -80,7 +80,7 @@ class IRCBot(Irc):
         return "name: %s nick: %s server: %s ipv6: %s ssl: %s port:%s" % (self.name, \
 self.nick, self.server, self.ipv6, self.ssl, self.port)
 
-    def _resume(self, data, reto):
+    def _resume(self, data, reto=None):
 
         """ resume the bot. """
 
