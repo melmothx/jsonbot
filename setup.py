a54a66dbb571f45352ed03ca512e8e2cbb004837
"Bart Thate ext:(%22) <bthate@gmail.com>
2010-05-17 19:25:02 +0200

    dont include curdir in sys.path .. supress warnings

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ upload = uploadlist('gaeupload')
 
 setup(
     name='jsonbot',
-    version='0.2b1',
+    version='0.2b2',
     url='http://jsonbot.googlecode.com/',
     download_url="http://code.google.com/p/jsonbot/downloads", 
     author='Bart Thate',
