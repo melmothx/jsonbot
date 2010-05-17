140c382fd7a011071400dc4e97df636fe6759532
"Bart Thate ext:(%22) <bthate@gmail.com>
2010-05-17 19:44:58 +0200

    remove eggs loading stuff

--- gozerlib/__init__.py
+++ gozerlib/__init__.py
@@ -8,14 +8,6 @@ __version__ = "0.2"
 
 __all__ = ['eventhandler', 'persistconfig', 'rest', 'jsbimport', 'admin', 'boot', 'botbase', 'callbacks', 'channelbase', 'commands', 'config', 'contrib', 'datadir', 'eggs', 'errors', 'eventbase', 'examples', 'fleet', 'gae', 'less', 'monitor', 'outputcache', 'periodical', 'persist', 'persiststate', 'plugins', 'plugs', 'runner', 'socklib', 'tasks', 'threadloop', 'threads', 'users', 'utils', 'console']
 
-import os
 import warnings
-
 warnings.simplefilter('ignore')
 
-from gozerlib.eggs import loadegg
-
-try:
-    from simplejson import loads
-except ImportError:
-    loadegg('simplejson', [os.getcwd(), ], log=True)
