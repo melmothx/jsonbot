0de26b4000eaf16fecc414c55eed2dcee9ade9a9
"Bart Thate ext:(%22) <bthate@gmail.com>
2010-05-21 00:16:56 +0200

    reenable reloadcheck

--- gozerlib/plugins.py
+++ gozerlib/plugins.py
@@ -127,7 +127,7 @@ class Plugins(LazyDict):
         """ dispatch event onto the cmnds object. check for pipelines first. """
         result = []
         if event.txt and not ' | ' in event.txt:
-            #self.needreloadcheck(bot, event)
+            self.needreloadcheck(bot, event)
             return cmnds.dispatch(bot, event, *args, **kwargs)
 
         if event.txt and ' | ' in event.txt:
