9a7c77ce4d0960d559a59796ba3291556bcbae9e
"Bart Thate ext:(%22) <bthate@gmail.com>
2010-06-03 20:57:59 +0200

    fix copying of __init__.py

--- gozerlib/datadir.py
+++ gozerlib/datadir.py
@@ -6,6 +6,7 @@
 
 ## basic imports
 
+import pkg_resources
 import re
 import os
 import shutil
@@ -25,10 +26,12 @@ def makedirs(ddir=None):
 
     if not os.path.isdir(ddir):
         os.mkdir(ddir)
-    shutil.copyfile(os.path.join("commonplugs", "__init__.py"), os.path.join(ddir, "__init__.py"))
+    source = pkg_resources.resource_filename('commonplugs', '')
+    shutil.copyfile(source + os.sep + "__init__.py", ddir + os.sep + '__init__.py')
     if not os.path.isdir(ddir + os.sep + 'myplugs'):
         os.mkdir(ddir + os.sep + 'myplugs')
-    shutil.copyfile(os.path.join("commonplugs", "__init__.py"), os.path.join(ddir, "myplugs", "__init__.py"))
+    source = pkg_resources.resource_filename('commonplugs', '')
+    shutil.copyfile(source + os.sep + "__init__.py", os.path.join(ddir,'myplugs', '__init__.py'))
     if not os.path.isdir(ddir + '/config/'):
         os.mkdir(ddir + '/config/')
     if not os.path.isdir(ddir + '/users/'):
