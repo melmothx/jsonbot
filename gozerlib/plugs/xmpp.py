0b9810227b91b7ee7bb58cee2dccec992c752768
"Bart Thate ext:(%22) <bthate@gmail.com>
2010-06-13 04:06:42 +0200

    add xmpp plugin

---
+++ gozerlib/plugs/xmpp.py
@@ -0,0 +1,29 @@
+# gozerlib/plugs/xmpp.py
+#
+#
+
+""" xmpp related commands. """
+
+## gozerlib imports
+
+from gozerlib.commands import cmnds
+from gozerlib.examples import examples
+from gozerlib.fleet import fleet
+
+## commands
+
+def handle_xmppinvite(bot, event):
+    """ invite (subscribe to) a different user. """
+    if not event.rest:
+        event.missing("<list of jids>")
+        return
+    bot = fleet.getfirstjabber()
+    if bot:
+        for jid in event.args:
+            bot.invite(jid)
+        event.done()
+    else:
+        event.reply("can't find jabber bot in fleet")
+
+cmnds.add("xmpp-invite", handle_xmppinvite, 'OPER')
+examples.add("xmpp-invite", "invite a user.", "xmpp-invite jsoncloud@appspot.com")
