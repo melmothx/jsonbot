4eb1f2eebaf2f09cc9ca933b609d5cbd8cb82b63
"Bart Thate ext:(%22) <bthate@gmail.com>
2010-07-03 05:31:49 +0200

    add gozerevent and container modules

---
+++ gozerlib/container.py
@@ -0,0 +1,45 @@
+# gozerlib/container.py
+#
+#
+
+""" container for bot to bot communication. """
+
+## gozerlib imports
+
+from gozerlib.gozerevent import GozerEvent
+
+## basic imports
+
+import uuid
+import time
+
+## defines
+
+idattributes = ['origin', 'type', 'payload', 'idtime']
+
+## functions
+
+def getid(container):
+    name = ""
+    for attr in idattributes:
+        try:
+            name += str(container[attr])
+        except KeyError:
+            pass
+    return uuid.uuid3(uuid.NAMESPACE_URL, name).hex
+
+## classes
+
+class Container(GozerEvent):
+
+    def __init__(self, origin=None, payload=None, type="event"):
+        GozerEvent.__init__(self)
+        self.createtime = time.time()
+        self.origin = origin
+        self.payload = payload
+        self.type = str(type) 
+        self.makeid()
+
+    def makeid(self):
+        self.idtime = time.time()
+        self.id = getid(self)
