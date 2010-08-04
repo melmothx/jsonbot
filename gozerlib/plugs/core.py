abddfb2996703f1238bb78377e0fc16b47d7cc7e
"Bart Thate ext:(%22) <bthate@gmail.com>
2010-08-04 20:21:43 +0200

    use , as seperator not -

--- gozerlib/plugs/core.py
+++ gozerlib/plugs/core.py
@@ -395,7 +395,7 @@ cmnds.add('threads', handle_threads, ['USER', 'OPER'])
 examples.add('threads', 'show running threads', 'threads')
 
 def handle_statusline(bot, event):
-    event.reply("modfied: %s - feeds: %s" % (time.ctime(event.chan.lastmodified), " - ".join(event.chan.data.feeds)))
+    event.reply("modfied: %s - feeds: %s" % (time.ctime(event.chan.lastmodified), ", ".join(event.chan.data.feeds)))
 
 cmnds.add('statusline', handle_statusline, ['OPER', 'USER', 'GUEST'])
 examples.add('statusline', 'show status line', 'statusline')
