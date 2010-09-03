acfef99d597a8a5b90178fe408fecf2ac8d99475
"Bart Thate ext:(%22) <bthate@gmail.com>
2010-09-03 23:04:01 +0200

    add forget command to learn plugin

--- commonplugs/learn.py
+++ commonplugs/learn.py
@@ -24,7 +24,7 @@ def handle_learn(bot, event):
         event.missing("<item> is <description>")
         return
     try:
-        (what, i, description) = event.rest.split(" ", 2)
+        (what, description) = event.rest.split(" is ")
     except ValueError:
         event.missing("<item> is <description>")
         return
@@ -37,17 +37,42 @@ def handle_learn(bot, event):
     if description not in items.data[what]:
         items.data[what].append(description)
     items.save()
-    event.reply("item added to %s" % event.channel)
+    event.reply("item added to %s database" % event.channel)
 
-cmnds.add('learn', handle_learn, ['USER', 'OPER', 'GUEST'])
+cmnds.add('learn', handle_learn, ['USER', 'OPER'])
 cmnds.add('=', handle_learn, ['USER', 'OPER'])
 examples.add('learn', 'learn the bot a description of an item.', "learn dunk is botpapa")
 
+def handle_forget(bot, event):
+    """" set an information item. """
+    if not event.rest:
+        event.missing("<item> and <match>")
+        return
+    try:
+        (what, match) = event.rest.split(" and ", 2)
+    except ValueError:
+        event.missing("<item> and <match>")
+        return
+        
+    items = PlugPersist(event.channel)
+    if not items.data:
+        items.data = LazyDict()
+    if items.data.has_key(what):
+        for i in range(len(items.data[what])):
+            if match in items.data[what][i]:
+                del items.data[what][i]                
+                items.save()
+                break
+    event.reply("item removed from %s database" % event.channel)
+
+cmnds.add('forget', handle_forget, ['USER', 'OPER'])
+examples.add('forget', 'forget a description of an item.', "forget dunk and botpapa")
+
 def handle_whatis(bot, event):
     items = PlugPersist(event.channel)
-    if items.data.has_key(event.rest):
+    if items.data.has_key(event.rest) and items.data[event.rest]:
         event.reply("%s is " % event.rest, items.data[event.rest], dot=", ")
-    elif event.chan.data.info and event.chan.data.info.has_key(event.rest):
+    elif event.chan.data.info and event.chan.data.info.has_key(event.rest) and event.chan.info[event.rest]:
         event.reply("%s is " % event.rest, event.chan.data.info[event.rest], dot=", ")
     else:
         event.reply("no information known about %s" % event.rest)
@@ -63,10 +88,14 @@ def prelearn(bot, event):
 
 def learncb(bot, event):
     items = PlugPersist(event.channel)
-    if items.data.has_key(event.rest):
-        event.reply("%s is " % event.rest, items.data[event.rest], dot=", ")
-    elif event.chan.data.info and event.chan.data.info.has_key(event.rest):
-        event.reply("%s is " % event.rest, event.chan.data.info[event.rest], dot=", ")
+    try:
+        target = event.usercmnd[1:]
+    except IndexError:
+        return
+    if items.data.has_key(target):
+        event.reply("%s is " % target, items.data[target], dot=", ")
+    elif event.chan.data.info and event.chan.data.info.has_key(target):
+        event.reply("%s is " % target, event.chan.data.info[target], dot=", ")
 
 callbacks.add("PRIVMSG", learncb, prelearn)
 callbacks.add("MESSAGE", learncb, prelearn)
