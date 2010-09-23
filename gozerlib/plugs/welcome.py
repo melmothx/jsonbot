86f4115677b38f14ab5bce7bc4a397405829b8c8
Bart Thate <bthate@gmail.com>
2010-09-23 13:19:33 +0200

    fix typo

--- gozerlib/plugs/welcome.py
+++ gozerlib/plugs/welcome.py
@@ -15,4 +15,4 @@ def handle_welcome(bot, event):
     event.reply("Welcome to JSONBOT .. The JSON everywhere bot ;] for wave/web/xmpp/IRC/console - you can give this bot commands. try !help.")
 
 cmnds.add('welcome', handle_welcome, ['USER', 'OPER', 'GUEST'])
-exmaples.add('welcome', 'send welcome msg', 'welcome')
+examples.add('welcome', 'send welcome msg', 'welcome')
