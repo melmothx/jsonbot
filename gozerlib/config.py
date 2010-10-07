4b6b5a06fd43ec3d792890bcba0d45634a5c40d8
Bart Thate <bthate@gmail.com>
2010-10-07 13:26:36 +0200

    reenable the version command

--- gozerlib/config.py
+++ gozerlib/config.py
@@ -186,8 +186,7 @@ class Config(LazyDict):
             self.setdefault('floodallow', 0)
             self.setdefault('auto_register', 0)
             self.setdefault('ondemand', 1)
-
-        self['version'] = "JSONBOT 0.5 DEVELOPMENT"
+            self['version'] = "JSONBOT 0.5 DEVELOPMENT"
         self['createdfrom'] = whichmodule()
 
     def reload(self):
