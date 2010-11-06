7f6b2dd65e2b4efe81f22096ffede5a54828729b
Bart Thate <bthate@gmail.com>
2010-11-06 13:11:12 +0100

    add jsb-dotip

--- gozerlib/version.py
+++ gozerlib/version.py
@@ -4,11 +4,25 @@
 
 """ version related stuff. """
 
+## gozerlib imports
+
+from gozerlib.datadir import getdatadir
+
+## basic imports
+
+import os
+
+## defines
+
 version = "0.5 RC1"
 
 ## getversion function
 
 def getversion(txt=""):
     """ return a version string. """
+    try: tip = open(getdatadir() + os.sep + "TIP", 'r').read()
+    except: tip = None
+    global version
+    if tip: version += " " + tip
     if txt: return "JSONBOT %s %s" % (version, txt)
     else: return "JSONBOT %s" % version
