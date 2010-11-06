c001f3ae19b763914805c7842fa2701f02dd90d6
Bart Thate <bthate@gmail.com>
2010-11-06 20:18:19 +0100

    use bot.cfg.followlist only on GAE

--- commonplugs/idle.py
+++ commonplugs/idle.py
@@ -29,7 +29,7 @@ if not idle.data:
 
 def preidle(bot, event):
     """ idle precondition aka check if it is not a command """
-    if event.userhost not in bot.cfg.followlist:
+    if bot.isgae and event.userhost not in bot.cfg.followlist:
         return False
     else:
         return True
