4ea66e59d88909fb0f57650f24aef0172d555b08
Bart Thate <bthate@gmail.com>
2010-11-16 12:58:22 +0100

    sleep 0.3 seconds before showing console prompt

--- gozerlib/console/bot.py
+++ gozerlib/console/bot.py
@@ -95,7 +95,7 @@ class ConsoleBot(BotBase):
                     if not result: continue
                     logging.debug("console - waiting for %s to finish" % event.usercmnd)
                     res = waitforqueue(event.outqueue)
-                    time.sleep(0.1)
+                    time.sleep(0.3)
                     logging.warn("console - %s" % res)
                 except NoSuchCommand: print "no such command: %s" % event.usercmnd
             except NoInput: continue
