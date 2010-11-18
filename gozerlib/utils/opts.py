ebb1cd1dbda081c3d127b21c59b55e81e816deb4
"Bart Thate ext:(%22) <bthate@gmail.com>
2010-11-18 22:09:50 +0100

    add jsb-irc.1 and jsb-xmpp.1 manual pages

--- gozerlib/utils/opts.py
+++ gozerlib/utils/opts.py
@@ -30,7 +30,30 @@ class EventOptionParser(optparse.OptionParser):
 
 ## makeopts function
 
-def makeopts(txt=""):
+def makeircopts(txt=""):
+    """ create commandline parser options. """
+    parser = optparse.OptionParser(usage='usage: %prog [options]', version=getversion())
+    parser.add_option('', '-r', type='string', default=False, dest='doresume',  metavar='PATH', 
+                  help="resume the bot from the folder specified")
+    parser.add_option('-d', '--datadir', type='string', default=False, dest='datadir',  help="datadir of the bot")
+    parser.add_option('-o', '--owner', type='string', default=False, dest='owner',  help="owner of the bot")
+    parser.add_option('-s', '--server', type='string', default=False, dest='server',  help="server to connect to (irc)")
+    parser.add_option('-c', '--channel', type='string', default=False, dest='channel',  help="channel to join")
+    parser.add_option('-l', '--loglevel', type='string', default="", dest='loglevel',  help="loglevel of the bot")
+    parser.add_option('-p', '--password', type='string', default=False, dest='password', help="set password used to connect to the server")
+    parser.add_option('', '--name', type='string', default=False, dest='name', help="bot's name")
+    parser.add_option('', '--port', type='string', default=False, dest='port',  help="set port of server to connect to")
+    parser.add_option('', '--save', action='store_true', default=False, dest='save',  help="save to config file")
+    parser.add_option('-n', '--nick', type='string', default=False, dest='nick',  help="nick of the bot")
+    parser.add_option('', '--ssl', action='store_true', default=False, dest='ssl',  help="use ssl")
+    parser.add_option('-y', '--nossl', action='store_true', default=False, dest='nossl',  help="don't use ssl")
+    parser.add_option('-6', '--ipv6', action='store_true', default=False, dest='ipv6', help="enable ipv6 bot")
+    if txt: opts, args = parser.parse_args(txt.split())
+    else: opts, args = parser.parse_args()
+    opts.args = args
+    return opts
+
+def makexmppopts(txt=""):
     """ create commandline parser options. """
     parser = optparse.OptionParser(usage='usage: %prog [options]', version=getversion())
     parser.add_option('', '-r', type='string', default=False, dest='doresume',  metavar='PATH', 
@@ -46,13 +69,6 @@ def makeopts(txt=""):
     parser.add_option('', '--port', type='string', default=False, dest='port',  help="set port of server to connect to")
     parser.add_option('', '--save', action='store_true', default=False, dest='save',  help="save to config file")
     parser.add_option('-n', '--nick', type='string', default=False, dest='nick',  help="nick of the bot")
-    parser.add_option('-a', '--all', action='store_true', default=False, dest='all', help="show available fleet bots")
-    parser.add_option('', '--ssl', action='store_true', default=False, dest='ssl',  help="use ssl")
-    parser.add_option('-y', '--nossl', action='store_true', default=False, dest='nossl',  help="don't use ssl")
-    parser.add_option('-x', '--exec', type='string', default="", dest='command', help="give a command to execute")
-    parser.add_option('-t', '--type', type='string', default="console", dest='type', help="define type of the bot")
-    parser.add_option('-z', '--forward', action='store_true', default=False, dest='forward', help="enable forwarding bot")
-    parser.add_option('-6', '--ipv6', action='store_true', default=False, dest='ipv6', help="enable ipv6 bot")
     if txt: opts, args = parser.parse_args(txt.split())
     else: opts, args = parser.parse_args()
     opts.args = args
@@ -114,7 +130,28 @@ def makeconsoleconfig(type, opts, botname=None):
     else: cfg.loglevel = cfg.loglevel or "error"
     return cfg
 
-def makeconfig(type, opts, botname=None):
+def makeircconfig(type, opts, botname=None):
+    """ make config file based on options. """
+    if not botname: botname = opts.name or "default-%s" % str(type)
+    botname = stripname(botname)
+    cfg = Config('fleet' + os.sep + botname + os.sep + 'config')
+    cfg.type = type
+    cfg.botname = botname
+    if opts.password: cfg.password = opts.password
+    if opts.ssl: cfg.ssl = True
+    if opts.nossl: cfg.ssl = False
+    if opts.port: cfg.port = opts.port
+    else: cfg.port = 6667
+    if opts.server: cfg.server = opts.server
+    else: cfg.server = cfg.server or "localhost"
+    if not cfg.owner: cfg.owner = []
+    if opts.owner and opts.owner not in cfg.owner: cfg.owner.append(opts.owner)
+    if opts.loglevel: cfg.loglevel = opts.loglevel
+    else: cfg.loglevel = cfg.loglevel or "warn"
+    if opts.ipv6: cfg.ipv6 = opts.ipv6
+    return cfg
+
+def makexmppconfig(type, opts, botname=None):
     """ make config file based on options. """
     if not botname: botname = opts.name or "default-%s" % str(type)
     botname = stripname(botname)
@@ -130,10 +167,6 @@ def makeconfig(type, opts, botname=None):
         try: cfg.host = cfg.user.split('@')[1]
         except ValueError: print "user is not in the nick@server format"
     if opts.password: cfg.password = opts.password
-    if opts.ssl: cfg.ssl = True
-    if opts.nossl: cfg.ssl = False
-    if opts.port: cfg.port = opts.port
-    else: cfg.port = 6667
     if opts.server: cfg.server = opts.server
     else: cfg.server = cfg.server or "localhost"
     if opts.name: cfg.jid = opts.name
@@ -141,5 +174,4 @@ def makeconfig(type, opts, botname=None):
     if opts.owner and opts.owner not in cfg.owner: cfg.owner.append(opts.owner)
     if opts.loglevel: cfg.loglevel = opts.loglevel
     else: cfg.loglevel = cfg.loglevel or "warn"
-    if opts.ipv6: cfg.ipv6 = opts.ipv6
     return cfg
