ee9520ad909149e814e3e9be37809b07b450a3a1
Bart Thate <bthate@gmail.com>
2010-12-13 23:23:07 +0100

    create datadir/config if it doesnt exist yet

--- gozerlib/datadir.py
+++ gozerlib/datadir.py
@@ -50,7 +50,7 @@ def makedirs(ddir=None):
             try:
                 source = "gozerdata"
                 shutil.copytree(source, ddir)
-            except OSError: 
+            except IOError: 
                 try:
                     source = "/var/lib/jsonbot/gozerdata"
                     shutil.copytree(source, ddir)
@@ -62,9 +62,9 @@ def makedirs(ddir=None):
             shutil.copytree(source, ddir + os.sep + 'myplugs')
         except ImportError: 
             try:
-                source = "gozerdata/myplugs"
+                source = "gozerdata" + os.sep + "myplugs"
                 shutil.copytree(source, ddir + os.sep + "myplugs")
-            except OSError: 
+            except IOError: 
                 try:
                     source = "/var/lib/jsonbot/gozerdata/myplugs"
                     shutil.copytree(source, ddir + os.sep + "myplugs")
@@ -76,7 +76,7 @@ def makedirs(ddir=None):
             shutil.copytree(source, ddir + os.sep + 'examples')
         except ImportError: 
             try:
-                source = "gozerdata/examples"
+                source = "gozerdata" + os.sep + "examples"
                 shutil.copytree(source, ddir + os.sep + "examples")
             except OStError: 
                 try:
@@ -84,19 +84,20 @@ def makedirs(ddir=None):
                     shutil.copytree(source, ddir + os.sep + "examples")
                 except: logging.error("datadir - failed to copy gozerdata/examples")
     if not os.path.isdir(ddir + os.sep + 'config'):
+        os.mkdir(ddir + os.sep + 'config')
         try:
             import pkg_resources
             source = pkg_resources.resource_filename('gozerdata', 'examples')
-            shutil.copytree(source, ddir + os.sep + 'config')
-        except ImportError: 
+            shutil.copy(source + os.sep + 'mainconfig.example', ddir + os.sep + 'config' + os.sep + 'mainconfig')
+        except (ImportError, IOError): 
             try:
-                source = "gozerdata/examples"
-                shutil.copytree(source, ddir + os.sep + "config")
-            except OSError: 
+                source = "gozerdata" + os.sep + "examples"
+                shutil.copy(source + os.sep + 'mainconfig.example', ddir + os.sep + 'config' + os.sep + 'mainconfig')
+            except IOError: 
                 try:
                     source = "/var/lib/jsonbot/gozerdata/examples"
-                    shutil.copytree(source, ddir + os.sep + "config")
-                except: logging.error("datadir - failed to copy gozerdata/myplugs")
+                    shutil.copy(source + os.sep + 'mainconfig.example', ddir + os.sep + 'config' + os.sep + 'mainconfig')
+                except: logging.error("datadir - failed to copy gozerdata/examples/mainconfig.example")
     try:
         import pkg_resources
         source = pkg_resources.resource_filename('commonplugs', '')
@@ -105,7 +106,7 @@ def makedirs(ddir=None):
         try:
             source = "commonplugs"
             shutil.copyfile(source + os.sep + "__init__.py", ddir + os.sep + '__init__.py')
-        except OSError: pass 
+        except IOError: pass 
     if not os.path.isdir(ddir + os.sep + 'myplugs'): os.mkdir(ddir + os.sep + 'myplugs')
     try:
         import pkg_resources
@@ -115,12 +116,10 @@ def makedirs(ddir=None):
         try:
             source = "commonplugs"
             shutil.copyfile(source + os.sep + "__init__.py", os.path.join(ddir,'myplugs', '__init__.py'))
-        except OSError: pass
+        except IOError: pass
     if not os.path.isdir(homedir + os.sep + '.jsonbot' + os.sep +'botlogs'): os.mkdir(homedir + os.sep + '.jsonbot' + os.sep + 'botlogs')
     if not os.path.isdir(ddir + '/run/'): os.mkdir(ddir + '/run/')
-    if not os.path.isdir(ddir + '/run/'): os.mkdir(ddir + '/run/')
     if not os.path.isdir(ddir + '/examples/'): os.mkdir(ddir + '/examples/')
-    if not os.path.isdir(ddir + '/config/'): os.mkdir(ddir + '/config/')
     if not os.path.isdir(ddir + '/users/'): os.mkdir(ddir + '/users/')
     if not os.path.isdir(ddir + '/channels/'): os.mkdir(ddir + '/channels/')
     if not os.path.isdir(ddir + '/fleet/'): os.mkdir(ddir + '/fleet/')
