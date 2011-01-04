ccfdd8a569dd27dffddcd76f0e7a2a751dde16a0
Bart Thate <bthate@gmail.com>
2011-01-04 11:31:15 +0100

    dont use Queue.task_done() yet

--- jsb/lib/runner.py
+++ jsb/lib/runner.py
@@ -78,7 +78,6 @@ class BotEventRunner(Runner):
                 logging.debug("closing %s queues" % len(ievent.queues))
                 for queue in ievent.queues: queue.put_nowait(None)
             if not ievent.dontclose: ievent.outqueue.put_nowait(None)
-            ievent.outqueue.task_done()
             ievent.inqueue.put_nowait(None)
             self.finished = time.time()
             self.elapsed = self.finished - self.starttime
