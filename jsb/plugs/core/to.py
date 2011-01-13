e55bd957402189744eca0377f4b37b0e76d1d144
Bart Thate <bthate@gmail.com>
2011-01-13 04:05:44 +0100

    add to plugin

---
+++ jsb/plugs/core/to.py
@@ -0,0 +1,31 @@
+# jsb/plugs/core/to.py
+#
+#
+
+""" send output to another user .. used in a pipeline. """
+
+## jsb imports
+
+from jsb.lib.commands import cmnds
+from jsb.utils.generic import getwho, waitforqueue
+from jsb.lib.examples import examples
+
+## to commands
+
+def handle_to(bot, ievent):
+    """ direct pipeline output to <nick>. """
+    if not ievent.inqueue: ievent.reply('use to in a pipeline') ; return
+    try: nick = ievent.args[0]
+    except IndexError: ievent.reply('to <nick>') ; return
+    if nick == 'me': nick = ievent.nick
+    if not getwho(bot, nick): ievent.reply("don't know %s" % nick) ; return
+    result = waitforqueue(ievent.inqueue, 5000)
+    if result:
+        ievent.reply("%s sends you this:" % ievent.nick, nick=nick)
+        ievent.reply(result, nick=nick, dot=True)
+        if len(result) == 1: ievent.reply('1 element sent')
+        else: ievent.reply('%s elements sent' % len(result))
+    else: ievent.reply('nothing to send')
+
+cmnds.add('to', handle_to, ['OPER', 'TO'])
+examples.add('to', 'send pipeline output to another user', 'list ! to dunker')
