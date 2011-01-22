a13cf29d9b6f8d28b9b2e95092a6bfe10402296b
Bart Thate <bthate@gmail.com>
2011-01-22 18:26:52 +0100

    add file to put into crontab .. check every 30 sec if the bot is still running.

---
+++ files/jsonbot_start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# use this in your crontab and copy this script
+#
+#*/2     *       *       *       *       jsb-fleet         /usr/bin/lockf -s -t0 /tmp/.bot /usr/local/bin/jsonbot_start.sh >> /dev/null 2>&;1
+
+PATH=bin:$PATH
+PIDFILE=~/.jsb/run/jsb.pid
+PID=`cat $PIDFILE` 2>/dev/null
+
+test -n "$PID" && ps auxw | grep -v grep | grep jsb-fleet | grep -q -s " $PID " && return 0
+jsb-fleet
+return 1
