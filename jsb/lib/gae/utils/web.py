b6c57b057cdf10a6d586add3f97067570005296d
Bart Thate <bthate@gmail.com>
2011-03-08 20:25:14 +0100

    make default color red (mildly)

--- jsb/lib/gae/utils/web.py
+++ jsb/lib/gae/utils/web.py
@@ -58,7 +58,7 @@ def start(response, input={}):
     except AttributeError:
          if os.environ.get('HTTP_HOST'): host = os.environ['HTTP_HOST']
          else: host = os.environ['SERVER_NAME']
-    inputdict = {'version': getversion(), 'host': host, 'color': Config().color or "#FDFA43"}
+    inputdict = {'version': getversion(), 'host': host, 'color': Config().color or "#CE1C1C"}
     if input: inputdict.update(input)
     temp = os.path.join(os.getcwd(), 'templates/console.html')
     from google.appengine.ext.webapp import template
@@ -73,7 +73,7 @@ def login(response, input={}):
     except AttributeError:
          if os.environ.get('HTTP_HOST'): host = os.environ['HTTP_HOST']
          else: host = os.environ['SERVER_NAME']
-    inputdict = {'version': getversion(), 'host': host, 'color': "#FFDA05"}
+    inputdict = {'version': getversion(), 'host': host, 'color': Config().color or "#CE1C1C"}
     if input: inputdict.update(input)
     temp = os.path.join(os.getcwd(), 'templates/login.html')
     from google.appengine.ext.webapp import template
