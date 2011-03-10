8e3f1e564dce81341c33e6643e8ef3d7bee14f1c
Bart Thate <bthate@gmail.com>
2011-03-10 01:42:04 +0100

    style final

--- jsb/lib/gae/utils/web.py
+++ jsb/lib/gae/utils/web.py
@@ -58,7 +58,7 @@ def start(response, input={}):
     except AttributeError:
          if os.environ.get('HTTP_HOST'): host = os.environ['HTTP_HOST']
          else: host = os.environ['SERVER_NAME']
-    inputdict = {'version': getversion(), 'host': host, 'color': Config().color or "#CE1C1C"}
+    inputdict = {'version': getversion(), 'host': host, 'color': Config().color or "#C54848"}
     if input: inputdict.update(input)
     temp = os.path.join(os.getcwd(), 'templates/console.html')
     from google.appengine.ext.webapp import template
@@ -73,7 +73,7 @@ def login(response, input={}):
     except AttributeError:
          if os.environ.get('HTTP_HOST'): host = os.environ['HTTP_HOST']
          else: host = os.environ['SERVER_NAME']
-    inputdict = {'version': getversion(), 'host': host, 'color': Config().color or "#CE1C1C"}
+    inputdict = {'version': getversion(), 'host': host, 'color': Config().color or "#C54848"}
     if input: inputdict.update(input)
     temp = os.path.join(os.getcwd(), 'templates/login.html')
     from google.appengine.ext.webapp import template
